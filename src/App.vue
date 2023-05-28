4993512e93d94c89a7daff83b93d03e88c3871ef
CikSimon <1simoncik1@gmail.com>
2023-05-28 20:15:39 +0200

    commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div>
+      <header class="d-flex">
+        <div class="header-div"> 
+          <h1 style="font-weight: bold;">NOTES</h1>
+          <button class="b1">Add note</button>
+        </div>   
+      </header>  
+    </div>
+    <div class="cards-container">
+      <div class="card">
+        <p class="main-text"></p>
+        <p class="date"></p>
+        <p class="hour"></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  .container{
+    background-color: antiquewhite;
+    width: 100vw;
+    height: 100vh;
+    color: black;
+  }
+  .d-flex{
+    display: flex;
+  }
+  .header-div{
+    text-align: center;
+    justify-content: center;
+    display: flex;
+    
+  }
+</style>
